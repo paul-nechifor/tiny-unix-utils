20762de6c1b4ed4c50ad0934b0318ed0abbf69de
Paul Nechifor <paul@nechifor.net>
2015-10-22 09:02:59 -0400

    Add macros.

--- readme.md
+++ readme.md
@@ -12,8 +12,6 @@ Run the tests with:
 
 ## TODO
 
-* Convert every thing to the C calling convention.
-* Create a common location for common things like `sys_call_creat equ 8`.
 * Split tests into files and extract common testing utilities.
 
 ## License
--- src/cat.asm
+++ src/cat.asm
@@ -1,31 +1,37 @@
+%include '_macros.mac'
+
 section .bss
 
-buffer: resb 4096
+buffer: resb small_buf_size
 
 section .text
 
 global _start
 _start:
 
-  mov rax, 3
-  mov rbx, 0
+  ; Read from stdin into the buffer.
+  mov rax, syscall_read
+  mov rbx, stdin_fileno
   mov rcx, buffer
-  mov rdx, 4096
-  int 0x80
+  mov rdx, small_buf_size
+  syscall
 
+  ; Exit if an error occured or there isn't anything left to read.
   cmp rax, 0
   jle exit
 
+  ; Store the number of bytes read into `rdx`.
   mov rdx, rax
 
-  mov rax, 4
-  mov rbx, 1
+  ; Write the same number of bytes to stdout.
+  mov rax, syscall_write
+  mov rbx, stdout_fileno
   mov rcx, buffer
-  int 0x80
+  syscall
 
   jmp _start
 
 exit:
-  mov rax, 1
-  mov rbx, 0
-  int 0x80
+  mov rax, syscall_exit
+  mov rbx, exit_success_code
+  syscall
