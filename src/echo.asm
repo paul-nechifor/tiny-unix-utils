5347fa1c15021b2226e1441ae215c2aafa3c830b
Paul Nechifor <paul@nechifor.net>
2015-11-04 09:01:19 -0500

    Make echo print more arguments.

--- src/echo.asm
+++ src/echo.asm
@@ -2,6 +2,8 @@
 
 section .data
 
+space_array: db chr_space
+
 section .text
 
 global _start
@@ -9,13 +11,35 @@ _start:
 
   ; `rsp` is argc
   ; `rsp + 8` is argv[0]
-  mov rax, 1
-  mov rdi, 1
-  mov rsi, [rsp + 16]
+  mov rbp, [rsp]
+  xor rbx, rbx
+  inc rbx
+
+print_next_one:
+  cmp rbx, rbp
+  jae exit
+
+  ; Write the first character.
+  mov rax, syscall_write
+  mov rdi, stdout_fileno
+  mov rsi, [rsp + 8 * rbx + 8]
   mov rdx, 1
   syscall
 
+  inc rbx
+  cmp rbx, rbp
+  jae exit
+
+  ; Write a space.
+  mov rax, syscall_write
+  mov rdi, stdout_fileno
+  mov rsi, space_array
+  mov rdx, 1
+  syscall
+
+  jmp print_next_one
+
 exit:
-  mov rax, 60
+  mov rax, syscall_exit
   xor rdi, rdi
   syscall
--- test/echo.coffee
+++ test/echo.coffee
@@ -1,2 +1,4 @@
-#itShould 'work with no arguments', './echo', ''
+itShould 'work with no arguments', './echo', ''
 itShould 'work with a single char', './echo a', 'a'
+itShould 'work with two single char args', './echo a b', 'a b'
+itShould 'work with three single char args', './echo a b c', 'a b c'
